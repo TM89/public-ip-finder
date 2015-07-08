7666647bb457202a11214ad046abb3dc6e6567da
Torbjørn <torbjornmevik@gmail.com>
2015-07-08 13:22:15 +0200

    Create public_ip_check.py
    The script file that checks the public ip, compares it to the old public ip and sends a mail if they are not equal. Should be run as a scheduled task or cron job

---
+++ public_ip_check.py
@@ -0,0 +1,56 @@
+from urllib2 import urlopen
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+import os
+import smtplib
+
+#smtp-server setup
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+server.ehlo()
+
+#mail parameters
+username = 'themevik89@gmail.com'
+password = ''
+fromaddr = 'themevik89@gmail.com'
+toaddr = 'themevik89@gmail.com'
+
+#Static subject for this script
+subject = 'Public ip changed!'
+
+#getting ip
+old_ip = '0.0.0.0'
+public_ip = urlopen('http://ip.42.pl/raw').read()
+filename = 'publicip.txt'
+
+def sendmail(new_ip):
+	msg = MIMEMultipart()
+	msg['From'] = fromaddr
+	msg['To'] = toaddr
+	msg['Subject'] = subject
+	body = 'Your public ip has changed and is now: '+public_ip
+	msg.attach(MIMEText(body,'plain'))
+	
+	server.login(username, password)
+	text = msg.as_string()
+	server.sendmail(fromaddr, toaddr, text)
+
+if os.path.isfile(filename):
+	file = open(filename,'r+')
+	old_ip = file.read()
+	file.close()
+else:
+	file = open(filename,'w')
+	file.write(public_ip)
+	file.close()
+	sendmail(public_ip)
+	
+if public_ip != old_ip:
+	file = open(filename, 'w')
+	file.write(public_ip)
+	file.close()
+	sendmail(public_ip)	 
+	
+	
+
